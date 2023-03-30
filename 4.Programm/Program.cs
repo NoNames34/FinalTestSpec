fdd30e0e872715d32beb2cd723f9bafa5ecd0fb3
AntonVN <ridick4@yandex.ru>
2023-03-30 21:52:49 +0500

    add-programm

---
+++ 4.Programm/Program.cs
@@ -0,0 +1,44 @@
+﻿/* ЗАДАНИЕ:
+Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых либо меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма.
+При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+*/
+//
+//Решение задачи с использованием 2-х массивов
+//
+Console.Write("Введите элементов для создания массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayStrings = new string[size];
+for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i+1}-й элемент: ");
+        string element = Convert.ToString(Console.ReadLine());
+        arrayStrings[i] = element;
+    }
+
+string[] arrayFinal = new string[size];
+int len = 3;
+int pos = 0;
+
+for (int j = 0; j < size; j++)
+    {
+        if (arrayStrings[j].Length <= len)
+        {
+            arrayFinal[pos] = arrayStrings[j];
+            pos++;
+        }
+    }
+
+Console.WriteLine();
+PrintArray(arrayFinal);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
